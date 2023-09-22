14f52d20b4e5eb4dc7eb28a75a7279d003dc979e
ShockedPlot7560 <no-reply@tchallon.fr>
2023-09-22 18:03:30 +0200

    add Docker image

---
+++ Dockerfile
@@ -0,0 +1,66 @@
+# Build stage for compilation. Build tools like g++ will not be copied into the final stage to reduce image size.
+FROM ubuntu:22.04
+ARG PMMP_TAG=5.6.0
+ARG PMMP_REPO=pmmp/PocketMine-MP
+
+RUN test ! -z ${PMMP_TAG} || (echo "Missing build-arg PMMP_TAG" && false)
+
+RUN apt-get update && apt-get install --no-install-recommends -y curl ca-certificates build-essential m4 gzip bzip2 bison git cmake autoconf automake pkg-config libtool libtool-bin re2c
+
+RUN mkdir /build
+WORKDIR /build
+RUN git clone https://github.com/${PMMP_REPO}.git -b ${PMMP_TAG} --recursive .
+WORKDIR /build/build/php
+RUN curl -L https://github.com/pmmp/PHP-Binaries/releases/download/php-8.1-latest/PHP-Linux-x86_64-PM5.tar.gz | tar -xz --strip-components=1
+RUN ln -s /build/build/php/php7/bin/php /usr/bin/php
+
+WORKDIR /build
+RUN curl -L https://getcomposer.org/installer | php
+RUN mv composer.phar /usr/bin/composer
+
+RUN composer install --classmap-authoritative --no-dev --prefer-source
+RUN if test -d build/preprocessor; then php build/preprocessor/PreProcessor.php --path=src --multisize; else echo "Skipping preprocessor step (preprocessor not found)"; fi
+RUN php $(test -d vendor/pocketmine/bedrock-data && echo -n vendor/pocketmine/bedrock-data || echo -n src/pocketmine/resources/vanilla)/.minify_json.php
+
+RUN php -dphar.readonly=0 build/server-phar.php --git $(git rev-parse HEAD)
+# Just to make sure DevTools didn't false-positive-exit
+RUN test -f /build/PocketMine-MP.phar
+
+WORKDIR /build
+COPY ../ /build/unittest
+WORKDIR /build/unittest
+ENV COMPOSER_ALLOW_SUPERUSER=1
+RUN composer install --no-dev --prefer-source --no-interaction --optimize-autoloader
+RUN php -dphar.readonly=0 build/plugin-phar.php UnitTest
+
+FROM ubuntu:22.04
+LABEL maintainer="PMMP Team <team@pmmp.io>"
+
+RUN apt-get update && apt-get install --no-install-recommends -y ca-certificates wget
+
+RUN groupadd -g 1000 pocketmine
+RUN useradd -r -d /pocketmine -p "" -u 1000 -m -g pocketmine pocketmine
+
+WORKDIR /pocketmine
+COPY --from=0 /build/build/php/php7 /usr/php
+RUN grep -q '^extension_dir' /usr/php/bin/php.ini && \
+	sed -ibak "s{^extension_dir=.*{extension_dir=\"$(find /usr/php -name *debug-zts*)\"{" /usr/php/bin/php.ini || echo "extension_dir=\"$(find /usr/php -name *debug-zts*)\"" >> /usr/php/bin/php.ini
+RUN ln -s /usr/php/bin/php /usr/bin/php
+COPY --from=0 /build/PocketMine-MP.phar PocketMine-MP.phar
+COPY --from=0 /build/unittest/UnitTest.phar /pocketmine/default_plugins/UnitTest.phar
+ADD start.sh /usr/bin/start-pocketmine
+
+RUN mkdir /plugins /data
+RUN chown 1000:1000 /plugins /data . -R
+RUN chmod o+x /usr/bin/php
+
+USER pocketmine
+
+ENV TERM=xterm
+
+EXPOSE 19132/tcp
+EXPOSE 19132/udp
+
+VOLUME ["/data", "/plugins"]
+
+CMD ["start-pocketmine"]
---
+++ build/plugin-phar.php
@@ -0,0 +1,170 @@
+<?php
+
+/*
+ *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
+ *
+ *
+*/
+
+declare(strict_types=1);
+
+namespace build\plugin_phar;
+
+use FilesystemIterator;
+use Generator;
+use Phar;
+use PharException;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use RegexIterator;
+
+require \dirname(__DIR__) . '/vendor/autoload.php';
+
+/**
+ * @param string[] $strings
+ *
+ * @return string[]
+ */
+function preg_quote_array(array $strings, string $delim) : array {
+	return \array_map(
+		function (string $str) use ($delim) : string {
+			return \preg_quote($str, $delim);
+		},
+		$strings
+	);
+}
+
+/**
+ * @param string[] $includedPaths
+ * @param string[] $files
+ */
+function buildPhar(string $pharPath, string $basePath, array $includedPaths, array $files, string $stub, int $signatureAlgo = Phar::SHA1, ?int $compression = null) : array|Generator { // @phpstan-ignore-line
+	$basePath = \rtrim(\str_replace("/", DIRECTORY_SEPARATOR, $basePath), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+	$includedPaths = \array_map(function (string $path) : string {
+		return \rtrim(\str_replace("/", DIRECTORY_SEPARATOR, $path), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+	}, $includedPaths);
+	yield "Creating output file $pharPath";
+	if (\file_exists($pharPath)) {
+		yield "Phar file already exists, overwriting...";
+		try {
+			Phar::unlinkArchive($pharPath);
+		} catch (PharException $e) {
+			//unlinkArchive() doesn't like dodgy phars
+			\unlink($pharPath);
+		}
+	}
+
+	yield "Adding files...";
+
+	$start = \microtime(true);
+	$phar = new Phar($pharPath);
+	$phar->setStub($stub);
+	$phar->setSignatureAlgorithm($signatureAlgo);
+	$phar->startBuffering();
+
+	foreach ($files as $file) {
+		$phar->addFile($file);
+	}
+
+	//If paths contain any of these, they will be excluded
+	$excludedSubstrings = preg_quote_array([
+		\realpath($pharPath), //don't add the phar to itself
+	], '/');
+
+	$folderPatterns = preg_quote_array([
+		DIRECTORY_SEPARATOR . 'tests' . DIRECTORY_SEPARATOR,
+		DIRECTORY_SEPARATOR . '.' //"Hidden" files, git dirs etc
+	], '/');
+
+	//Only exclude these within the basedir, otherwise the project won't get built if it itself is in a directory that matches these patterns
+	$basePattern = \preg_quote(\rtrim($basePath, DIRECTORY_SEPARATOR), '/');
+	foreach ($folderPatterns as $p) {
+		$excludedSubstrings[] = $basePattern . '.*' . $p;
+	}
+
+	$regex = \sprintf(
+		'/^(?!.*(%s))^%s(%s).*/i',
+		\implode('|', $excludedSubstrings), //String may not contain any of these substrings
+		\preg_quote($basePath, '/'), //String must start with this path...
+		\implode('|', preg_quote_array($includedPaths, '/')) //... and must be followed by one of these relative paths, if any were specified. If none, this will produce a null capturing group which will allow anything.
+	);
+
+	$directory = new RecursiveDirectoryIterator($basePath, FilesystemIterator::SKIP_DOTS | FilesystemIterator::FOLLOW_SYMLINKS | FilesystemIterator::CURRENT_AS_PATHNAME); //can't use fileinfo because of symlinks
+	$iterator = new RecursiveIteratorIterator($directory);
+	$regexIterator = new RegexIterator($iterator, $regex);
+
+	$count = \count($phar->buildFromIterator($regexIterator, $basePath));
+
+	yield "Added $count files";
+
+	if ($compression !== null) {
+		yield "Compressing files...";
+		$phar->compressFiles($compression);
+		yield "Finished compression";
+	}
+	$phar->stopBuffering();
+
+	yield "Done in " . \round(\microtime(true) - $start, 3) . "s";
+}
+
+function main(string $outputName) : void {
+	if (\ini_get("phar.readonly") == 1) {
+		echo "Set phar.readonly to 0 with -dphar.readonly=0" . \PHP_EOL;
+		exit(1);
+	}
+	if (\file_exists(\dirname(__DIR__) . '/vendor/phpunit')) {
+		echo "Remove Composer dev dependencies before building (composer install --no-dev)" . \PHP_EOL;
+		exit(1);
+	}
+
+	foreach (buildPhar(
+		\getcwd() . DIRECTORY_SEPARATOR . $outputName . ".phar",
+		\dirname(__DIR__) . DIRECTORY_SEPARATOR,
+		[
+			'resources',
+			'src',
+			'vendor'
+		],
+		[
+			'plugin.yml'
+		],
+		<<<'STUB'
+<?php
+
+$tmpDir = sys_get_temp_dir();
+if(!is_readable($tmpDir) or !is_writable($tmpDir)){
+	echo "ERROR: tmpdir $tmpDir is not accessible." . PHP_EOL;
+	echo "Check that the directory exists, and that the current user has read/write permissions for it." . PHP_EOL;
+	echo "Alternatively, set 'sys_temp_dir' to a different directory in your php.ini file." . PHP_EOL;
+	exit(1);
+}
+
+__HALT_COMPILER();
+STUB,
+		Phar::SHA1,
+		Phar::GZ
+	) as $line) {
+		echo $line . \PHP_EOL;
+	}
+}
+
+$outputName = $argv[1] ?? null;
+if ($outputName === null) {
+	echo "Usage: php build.php <output>" . \PHP_EOL;
+	exit(1);
+}
+
+main($outputName);
