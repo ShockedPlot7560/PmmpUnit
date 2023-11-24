8f07ece19dad66fe8c9f32f7d040f27286771f10
ShockedPlot7560 <no-reply@tchallon.fr>
2023-11-24 13:47:25 +0100

    fix CS

--- tests/pmmpunit/suitetest/normal/tests/AwaitGeneratorDecoratorTest.php
+++ tests/pmmpunit/suitetest/normal/tests/AwaitGeneratorDecoratorTest.php
@@ -6,7 +6,6 @@ use Closure;
 use Exception;
 use Generator;
 use pocketmine\scheduler\AsyncTask;
-use pocketmine\scheduler\Task;
 use React\Promise\PromiseInterface;
 use ShockedPlot7560\PmmpUnit\framework\TestCase;
 use ShockedPlot7560\PmmpUnit\PmmpUnit;
@@ -25,8 +24,8 @@ class AwaitGeneratorDecoratorTest extends TestCase {
 		return $decorator->then(function () : void {
 			$this->assertTrue(true);
 		})->catch(function (Exception $e) : void {
-            $this->assertTrue(false, $e->getMessage());
-        });
+			$this->assertTrue(false, $e->getMessage());
+		});
 	}
 
 	/**
@@ -36,33 +35,33 @@ class AwaitGeneratorDecoratorTest extends TestCase {
 		$decorator = new AwaitGeneratorDecorator($this->asyncTask());
 
 		return $decorator->then(function () : void {
-            $this->assertTrue(true);
-        })->catch(function (Exception $e) : void {
-            $this->assertTrue(false, $e->getMessage());
-        });
+			$this->assertTrue(true);
+		})->catch(function (Exception $e) : void {
+			$this->assertTrue(false, $e->getMessage());
+		});
 	}
 
 	private function asyncTask() : Generator {
 		yield from Await::promise(function ($resolve, $reject) {
-            $task = new class($resolve, $reject) extends AsyncTask{
-                public function __construct(
-                    Closure $resolve,
-                    Closure $reject
-                ){
-                    $this->storeLocal("resolve", $resolve);
-                    $this->storeLocal("reject", $reject);
-                }
+			$task = new class($resolve, $reject) extends AsyncTask {
+				public function __construct(
+					Closure $resolve,
+					Closure $reject
+				) {
+					$this->storeLocal("resolve", $resolve);
+					$this->storeLocal("reject", $reject);
+				}
 
-                public function onRun(): void {
-                    sleep(1);
-                    $this->setResult(true);
-                }
+				public function onRun() : void {
+					sleep(1);
+					$this->setResult(true);
+				}
 
-                public function onCompletion(): void{
-                    ($this->fetchLocal("resolve"))($this->getResult());
-                }
-            };
-            PmmpUnit::getInstance()->getServer()->getAsyncPool()->submitTask($task);
+				public function onCompletion() : void {
+					($this->fetchLocal("resolve"))($this->getResult());
+				}
+			};
+			PmmpUnit::getInstance()->getServer()->getAsyncPool()->submitTask($task);
 		});
 	}
 }
