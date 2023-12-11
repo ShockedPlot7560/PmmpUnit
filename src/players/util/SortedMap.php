e594447648fb7a49f622470cadddb54a23dc83fe
ShockedPlot7560 <no-reply@tchallon.fr>
2023-12-11 11:22:09 +0100

    fix PHP 8.3 strange bloat bug

--- src/players/util/SortedMap.php
+++ src/players/util/SortedMap.php
@@ -38,7 +38,9 @@ final class SortedMap {
 	public function set(string|int $key, mixed $value, int $score) : void {
 		$this->entries[$key] = $value;
 		$this->key_scores[$key] = $score;
-		uksort($this->key_scores, fn (string $k1, string $k2) : int => $this->key_scores[$k1] <=> $this->key_scores[$k2]);
+        $scores = $this->key_scores;
+		uksort($scores, fn (string $k1, string $k2) : int => $this->key_scores[$k1] <=> $this->key_scores[$k2]);
+        $this->key_scores = $scores;
 	}
 
 	/**
